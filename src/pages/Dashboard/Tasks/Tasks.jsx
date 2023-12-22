09267002ac5b1a6ecf195a79843e03a6c3986129
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-12-22 19:57:33 +0600

    delete

--- src/pages/Dashboard/Tasks/Tasks.jsx
+++ src/pages/Dashboard/Tasks/Tasks.jsx
@@ -1,14 +1,38 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../context/AuthProvider";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const Tasks = () => {
     const { user } = useContext(AuthContext);
     const [tasks, setTasks] = useState([]);
+    const [assignments, setAssignments] = useState([]);
     useEffect(() => {
         axios(`https://taskia-server.vercel.app/task/${user?.email}`)
             .then(res => setTasks(res.data))
     }, [user?.email])
+    const handleDelete = (id) => {
+        console.log(id)
+        fetch(`https://taskia-server.vercel.app/delete-task/${id}?email=${user?.email}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.message) {
+                    toast.error('You Are Not Able to Delete')
+                }
+                else {
+
+                    // remove the user from the UI
+
+                    const remainingUsers = assignments && assignments.filter(item => (item._id !== id));
+                    setAssignments(remainingUsers);
+                    toast.success('Deleted Successfully');
+
+                }
+
+            })
+    }
     return (
         <div className="h-screen p-2">
             <div className="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-1 gap-5">
@@ -22,7 +46,7 @@ const Tasks = () => {
 
                     <div className="grid grid-rows-2 gap-2">
                         {
-                            tasks?.map(task => <div key={task.id} className="p-2 rounded shadow-sm border-gray-100 border-2">
+                            tasks?.map(task => <div key={task._id} className="p-2 rounded shadow-sm border-gray-100 border-2">
                                 <h3 className="text-sm mb-3 text-gray-700">{task.title}</h3>
                                 <p className="bg-red-100 text-xs w-max p-1 rounded mr-2 text-gray-700">To-do</p>
                                 <div className="flex flex-row items-center mt-2">
@@ -33,6 +57,7 @@ const Tasks = () => {
                                     <p className="text-xs text-gray-500 mt-2">Deadline: {task.deadline}</p>
                                     <p className="text-xs text-gray-500 mt-2">Priority Level: {task.pLevel}</p>
                                 </div>
+                                <button className="btn" onClick={()=>handleDelete(task._id)}>Delete</button>
                             </div>)
                         }
 
